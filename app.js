8ff4eb648865e5282f6c5af037be8a6adf68729c
Mark Rodriguez <Markrodriguez003@gmail.com>
2020-06-16 16:13:37 -0400

    Created inquirer prompt and finalized classes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,6 +9,187 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { Console } = require("console");
+const teamMembers = [];
+
+function generateTeam() {
+    console.log("Generating Team . . . ");
+    console.log("TEAM: ");
+    teamMembers.forEach(member => {
+        console.log(member);
+    })
+    
+    const x = render(teamMembers);
+    console.log(x);
+}
+
+function choiceLoop() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'loop',
+                message: 'What role would you like to create for your team?',
+                choices: ['Create another Team Member', `Generate Team`, "Exit"],
+            },
+        ])
+        .then(answers => {
+            console.log(answers);
+            if (answers.loop === "Create another Team Member") {
+                console.log("Creating new member . . . ");
+                teamBuilder();
+
+            } else if (answers.loop === "Generate Team") {
+                generateTeam();
+            } else return "Exiting. . . ";
+        })
+        .catch(error => {
+            if (error) {
+                console.log("Loop - Error");
+            } else {
+                console.log("Loop - Error 2");
+            }
+        });
+}
+
+function teamBuilder() {
+    console.log("**************************************************************************");
+    console.log("                         TEAM - MEMBER BUILDER");
+    console.log("**************************************************************************");
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'userChoice',
+                message: 'What role would you like to create for your team?',
+                choices: ['Intern', `Engineer`, 'Manager', "Exit"],
+            },
+        ])
+        .then(answers => {
+            console.log(answers);
+            if (answers.userChoice === "Intern") {
+                console.log("Creating new Intern member . . . ");
+                createIntern();
+            } else if (answers.userChoice === "Engineer") {
+                console.log("Creating new Engineer member . . . ");
+                createEngineer();
+            } else if (answers.userChoice === "Manager") {
+                console.log("Creating new Manager member . . . ");
+                createManager();
+            } else return "Exiting. . . ";
+        })
+        .catch(error => {
+            if (error) {
+                console.log("Error");
+            } else {
+                console.log("Error 2");
+            }
+        });
+}
+
+function createIntern() {
+    inquirer
+        .prompt([
+            {
+                name: 'name',
+                message: 'Name: ',
+            },
+            {
+                name: 'id',
+                message: 'Id: ',
+            },
+            {
+                name: 'email',
+                message: 'Email: ',
+            },
+            {
+                name: 'school',
+                message: 'School: ',
+            },
+        ])
+        .then(r => {
+            const newIntern = new Intern(r.name, r.id, r.email, r.school);
+            teamMembers.push(newIntern);
+            choiceLoop();
+        })
+        .catch(error => {
+            if (error) {
+                console.log("Intern - Error");
+            } else {
+                console.log("Intern - Error 2");
+            }
+        });
+}
+
+function createEngineer() {
+    inquirer
+        .prompt([
+            {
+                name: 'name',
+                message: 'Name: ',
+            },
+            {
+                name: 'id',
+                message: 'Id: ',
+            },
+            {
+                name: 'email',
+                message: 'Email: ',
+            },
+            {
+                name: 'github',
+                message: 'Github: ',
+            },
+        ])
+        .then(r => {
+            const newEng = new Engineer(r.name, r.id, r.email, r.github);
+            teamMembers.push(newEng);
+            choiceLoop();
+        })
+        .catch(error => {
+            if (error) {
+                console.log("Engineer - Error");
+            } else {
+                console.log("Engineer - Error 2");
+            }
+        });
+}
+
+function createManager() {
+    inquirer
+        .prompt([
+            {
+                name: 'name',
+                message: 'Name: ',
+            },
+            {
+                name: 'id',
+                message: 'Id: ',
+            },
+            {
+                name: 'email',
+                message: 'Email: ',
+            },
+            {
+                name: 'officenum',
+                message: 'Office Number: ',
+            },
+        ])
+        .then(r => {
+            const newMan = new Manager(r.name, r.id, r.email, r.officenum);
+            teamMembers.push(newMan);
+            choiceLoop();
+        })
+        .catch(error => {
+            if (error) {
+                console.log("Engineer - Error");
+            } else {
+                console.log("Engineer - Error 2");
+            }
+        });
+}
+
+teamBuilder();
 
 
 // Write code to use inquirer to gather information about the development team members,
